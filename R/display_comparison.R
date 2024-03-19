7a43afca278486954f4ce350614551b13d0d4306
RossanaTat <rossana.tatulli@studbocconi.it>
2024-03-19 18:05:06 -0400

    fix display_sync_status for common files

--- R/display_comparison.R
+++ R/display_comparison.R
@@ -4,28 +4,28 @@ library(DT)
 
 # Function to display directory comparison in DT table ####
 
-display_dt <- function(dir1,
-                       dir2,
-                       by = "date",
-                       dircomp = NULL) {
+display_sync_status <- function(
+                       sync_status) {
 
   #take as input either dir1_path and dir2_path or dircomp
 
-  if (is.null(dircomp)) {
-
-    dircomp <- compare_directories(dir1 = dir1,
-                                   dir2 = dir2,
-                                   by = by)$dir_compare
-
-  } else (dircomp <- dircomp$dir_compare)
+  # if (is.null(sync_status)) {
+  #
+  #   dircomp <- compare_directories(dir1 = dir1,
+  #                                  dir2 = dir2,
+  #                                  by = by)$dir_compare
+  #
+  # } else (dircomp <- dircomp$dir_compare)
 
   # Build DT table
 
-  DT::datatable(dircomp,
+  sync_status = sync_status$common_files
+
+  DT::datatable(sync_status,
                 options = list(
                   pageLength = 10, # number of rows to display per page
                   columnDefs = list(
-                  list(targets = grep("^is_", colnames(dircomp), value = TRUE),
+                  list(targets = grep("^is_", colnames(sync_status), value = TRUE),
                        createdCell = JS(
                        "function(td, cellData, rowData, row, col) {if (cellData === true) {
                                     $(td).css({'background-color': '#89CFF0'});
