ff6b8523697509baec7a82fc0c1d9ffd12d06f08
RossanaTat <rossana.tatulli@studbocconi.it>
2024-03-19 15:42:35 -0400

    fix utils

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 
 # functions to create temp folders and sub folders
 
-# function to create temporary folders with subfolders and R files
-create_temp_folders <- function(num_folders = 3,
-                                num_subfolders = 2,
+# function to create a temporary folder with 1 or more sub-folders
+create_temp_folders <- function(num_subfolders = 1,
+                                #num_subfolders = 2,
                                 name = "temp_folder") {
 
   # Create a temporary directory
@@ -15,42 +15,55 @@ create_temp_folders <- function(num_folders = 3,
 
 
   # Create folders
-  folder_names <- paste0("folder", "_", seq_len(num_folders))
+  folder_names <- paste0("subfolder", "_", seq_len(num_subfolders))
   folder_paths <- file.path(temp_dir, folder_names)
   lapply(folder_paths, dir.create)
 
   # Call create_subfolders function for each folder
-  lapply(folder_paths, create_subfolders, num_subfolders)
+  #lapply(folder_paths, create_subfolders, num_subfolders)
 
   # Return the path to the temporary directory
   return(temp_dir)
 }
 
 # Function to create subfolders and R files within a folder
-create_subfolders <- function(folder_path, num_subfolders) {
+# create_subfolders <- function(folder_path, num_subfolders, file_name) {
+#
+#   subfolder_names <- paste0("subfolder", "_", 1:num_subfolders)
+#
+#
+#   subfolder_paths <- file.path(folder_path, subfolder_names)
+#
+#   lapply(subfolder_paths, dir.create)
+#
+#   lapply(subfolder_paths,
+#          function(subfolder_path) file.create(file.path(subfolder_path, paste0("file", "_", file_name, ".R"))))
+# }
 
-  subfolder_names <- paste0("subfolder", "_", 1:num_subfolders)
 
+# Function to create file in folder ####
+create_temp_file <- function(folder_path, file_name) {
 
-  subfolder_paths <- file.path(folder_path, subfolder_names)
+  stopifnot(expr = {
+    fs::dir_exists(folder_path)
+  })
 
-  lapply(subfolder_paths, dir.create)
+  file_path <- file.path(folder_path, paste0("file", "_", file_name, ".R"))
+  file.create(file_path)
 
-  lapply(subfolder_paths,
-         function(subfolder_path) file.create(file.path(subfolder_path, paste0("file", "_", letters[seq_len(num_subfolders)], ".R"))))
+  # Return the path of the created file
+  return(file_path)
+
 }
-
-# TODO: Add function to add data to files/create files with data
-
-# Add sample data to R files in folders
-write_temporary_files <- function(folder_path, num_data_points, random = TRUE) {
+# Add sample data to R file in temp dir
+write_temporary_files <- function(file_path, num_data_points, random = TRUE) {
 
   #check path
   stopifnot(expr =
-              fs::dir_exists(folder_path))
+              fs::dir_exists(file_path))
 
   # Get all R files in the specified folder and subfolders
-  r_files <- list.files(folder_path, pattern = "\\.R$", recursive = TRUE, full.names = TRUE)
+  #r_files <- list.files(folder_path, pattern = "\\.R$", recursive = TRUE, full.names = TRUE)
 
   if (random == TRUE) {
 
@@ -61,12 +74,13 @@ write_temporary_files <- function(folder_path, num_data_points, random = TRUE) {
     {sample_data <- seq(1, num_data_points)}
 
     # Write random data to each R file
-  lapply(r_files, function(file) {
-    writeLines(sprintf("data <- c(%s)", paste(sample_data, collapse = ",")), file)
-  })
 
-  return(print("files succesfully written"))
+    writeLines(sprintf("data <- c(%s)", paste(sample_data, collapse = ",")), file_path)
+    return(print("file(s) succesfully written"))
 }
 
+## TODO: #######################################################################
+### Add one wrapper function to create folder-subfolder-file ####
+### Make these functions vectorized ####
 
 
