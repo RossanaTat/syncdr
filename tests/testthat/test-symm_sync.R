e69d59dca09bdee41804b400c1bd0469d5eaafb0
RossanaTat <rossana.tatulli@studbocconi.it>
2024-03-28 17:20:08 -0400

    update tests

--- tests/testthat/test-symm_sync.R
+++ tests/testthat/test-symm_sync.R
@@ -160,17 +160,18 @@ partial_symmetric_sync_common_files(sync_status,
 
 test_that("partial sym sync works -by date & cont", {
 
+  to_copy <- sync_status$common_files |>
+    fsubset(is_new_left | is_new_right) |>
+    fsubset(is_diff)
+
   sync_status_after <- compare_directories(left,
-                                           right)
+                                           right)$common_files |>
+    fsubset(path_left %in% to_copy$path_left &
+              path_right %in% to_copy$path_right)
 
-  cf_status_after <- sync_status_after$common_files |>
-    fselect(path_left, path_right, sync_status)
 
-  all(cf_status_after$sync_status == "same date") |>
-    expect_equal(TRUE)
-
-  all(compare_file_contents(cf_status_after$path_left,
-                            cf_status_after$path_right)$sync_status_content == "same content") |>
+  all(compare_file_contents(sync_status_after$path_left,
+                            sync_status_after$path_right)$sync_status == "same content") |>
     expect_equal(TRUE)
 
 })
