08d8c29c633cf001b7e9536fa8d79a4e76ddd1d1
RossanaTat <rossana.tatulli@studbocconi.it>
2024-07-05 17:46:24 +0200

    allow for left and right paths in sym sync functions

--- R/symmetric_sync.R
+++ R/symmetric_sync.R
@@ -10,7 +10,8 @@
 #' * For non common files:
 #'   - if a file exists in one but not in the other it is copied to the other directory
 #'
-#' @param sync_status object of class 'syncdr_status' with info on sync status and comparison of directories
+#' @param left_path Path to the left/first directory.
+#' @param right_path Path to the right/second directory.
 #' @param by_date logical, TRUE by default
 #' @param by_content logical, FALSE by default
 #' @param recurse logical, TRUE by default.
@@ -18,18 +19,25 @@
 #'  If the sub(directory) where the file is located does not exist in destination folder (or you are not sure), set recurse to FALSE,
 #'  and the file will be copied at the top level
 #' @param verbose logical. If TRUE, display directory tree before and after synchronization. Default is FALSE
-#' @return print "synchronized"
+#' @return Invisible TRUE indicating successful synchronization.
 #' @export
 #'
-full_symmetric_sync <- function(sync_status,
+full_symmetric_sync <- function(left_path,
+                                right_path,
                                 by_date    = TRUE,
                                 by_content = FALSE,
                                 recurse    = TRUE,
                                 verbose    = getOption("syncdr.verbose")) {
 
-  # Check sync_status is the result of compare_directories()
-  stopifnot(expr = {
-    inherits(sync_status, "syncdr_status")
+  # # Check sync_status is the result of compare_directories()
+  # stopifnot(expr = {
+  #   inherits(sync_status, "syncdr_status")
+  # })
+
+  # Check directory paths
+  stopifnot(exprs = {
+    fs::dir_exists(left_path)
+    fs::dir_exists(right_path)
   })
 
   # Inform user that sync by content only is not active and stop
@@ -37,12 +45,23 @@ full_symmetric_sync <- function(sync_status,
     cli::cli_abort(message = "Symmetric synchronization by content only is not active -no action will be executed, directories unchanged")
   }
 
+
+  # Get sync_status -internal call to compare_directories()
+  sync_status <- compare_directories(left_path  = left_path,
+                                     right_path = right_path,
+                                     by_date    = by_date,
+                                     by_content = by_content,
+                                     recurse    = recurse,
+                                     verbose    = verbose
+  )
+
+
   if (verbose == TRUE) {
   # Display folder structure before synchronization
   style_msgs(color_name = "blue",
                text = "Directories structure BEFORE synchronization:\n")
-  display_dir_tree(path_left  = sync_status$left_path,
-                   path_right = sync_status$right_path)
+  display_dir_tree(path_left  = left_path,
+                   path_right = right_path)
   }
 
   # Update non- and common files ###############################################
@@ -86,8 +105,8 @@ full_symmetric_sync <- function(sync_status,
     # Display folder structure AFTER synchronization
     style_msgs(color_name = "blue",
                text = "Directories structure AFTER synchronization:\n")
-    display_dir_tree(path_left  = sync_status$left_path,
-                     path_right = sync_status$right_path)
+    display_dir_tree(path_left  = left_path,
+                     path_right = right_path)
   }
 
   style_msgs(color_name = "green",
@@ -109,7 +128,8 @@ full_symmetric_sync <- function(sync_status,
 #'   - keep in right those that are only in right
 #'   - keep in left those that are only in left
 #'
-#' @param sync_status object of class 'syncdr_status' with info on sync status and comparison of directories
+#' @param left_path Path to the left/first directory.
+#' @param right_path Path to the right/second directory.
 #' @param by_date logical, TRUE by default
 #' @param by_content logical, FALSE by default
 #' @param recurse logical, TRUE by default.
@@ -117,31 +137,41 @@ full_symmetric_sync <- function(sync_status,
 #'  If the sub(directory) where the file is located does not exist in destination folder (or you are not sure), set recurse to FALSE,
 #'  and the file will be copied at the top level
 #' @param verbose logical. If TRUE, display directory tree before and after synchronization. Default is FALSE
-#' @return print "synchronized"
+#' @return Invisible TRUE indicating successful synchronization.
 #' @export
 partial_symmetric_sync_common_files <-
-  function(sync_status,
+  function(left_path,
+           right_path,
            by_date    = TRUE,
            by_content = FALSE,
            recurse    = TRUE,
            verbose    = getOption("syncdr.verbose")) {
 
-  # Check sync_status is the result of compare_directories()
-  stopifnot(expr = {
-    inherits(sync_status, "syncdr_status")
-  })
+    # Check directory paths
+    stopifnot(exprs = {
+      fs::dir_exists(left_path)
+      fs::dir_exists(right_path)
+    })
 
   # Inform user that sync by content only is not active
   if (by_date == FALSE & by_content == TRUE) {
     cli::cli_abort(message = "Symmetric synchronization by content only is not active -no action will be executed, directories unchanged")
   }
 
+  sync_status <- compare_directories(left_path  = left_path,
+                                       right_path = right_path,
+                                       by_date    = by_date,
+                                       by_content = by_content,
+                                       recurse    = recurse,
+                                       verbose    = verbose
+    )
+
   if(verbose == TRUE) {
 
     style_msgs(color_name = "blue",
                text = "Directories structure BEFORE synchronization:\n")
-    display_dir_tree(path_left  = sync_status$left_path,
-                     path_right = sync_status$right_path)
+    display_dir_tree(path_left  = left_path,
+                     path_right = right_path)
 
   }
   # Update non- and common files ###############################################
@@ -172,8 +202,8 @@ partial_symmetric_sync_common_files <-
   # Display folder structure AFTER synchronization
   style_msgs(color_name = "blue",
                text = "Directories structure AFTER synchronization:\n")
-  display_dir_tree(path_left  = sync_status$left_path,
-                   path_right = sync_status$right_path)}
+  display_dir_tree(path_left  = left_path,
+                   path_right = right_path)}
 
   style_msgs(color_name = "green",
              text = paste0("\u2714", " synchronized\n"))
